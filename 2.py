f546e13b707a8f9e87d4f9c3512e07f2808de658
grogreta3 <36796900+grogreta3@users.noreply.github.com>
2020-04-14 18:27:20 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,29 +1,31 @@
 def penz(d):
-    szotar={'100Ft':0,'50Ft':0,'10Ft':0,'5Ft':0,'2Ft':0,'1Ft':0}
+    szotar={'100':0,'50':0,'10':0,'5':0,'2':0,'1':0}
     while d!=0:
         n=int(input("adj meg egy szamot:"))
         while n!=0:
             if n>=100:
                 n=n-100
-                szotar['100Ft']+=1
+                szotar['100']+=1
             elif n>=50 and n<100:
                 n-=50
-                szotar['50Ft'] += 1
+                szotar['50'] += 1
             elif n>=10 and n<50:
                 n-=10
-                szotar['10Ft']+=1
+                szotar['10']+=1
             elif n>=5 and n<10:
                 n-=5
-                szotar['5Ft']+=1
+                szotar['5']+=1
             elif n>=2 and n<5:
                 n-=2
-                szotar['2Ft']+=1
+                szotar['2']+=1
             else:
                 n-=1
-                szotar['1Ft']+=1
+                szotar['1']+=1
         d-=1
-        print(szotar)
-        szotar = {'100Ft': 0,'50Ft' : 0,'10Ft':0,'5Ft':0 ,'2Ft':0 ,'1Ft':0}
+        for i in szotar:
+            if szotar[i]!=0:
+                print('{} db {} Ft'.format(szotar[i],i))
+        szotar = {'100': 0,'50' : 0,'10':0,'5':0 ,'2':0 ,'1':0}
 
 
 n=int(input("adj meg egy szamot hanyszor fusson a program:"))
